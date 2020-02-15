12d3436d7e224037e5808d778574f907ec4e7d4d
tbalden <illespal@gmail.com>
2020-02-15 12:30:43 -0800

    ntf_timer: fix in_call notif flashlight/vib

--- drivers/misc/notification/ntf_timer.c
+++ drivers/misc/notification/ntf_timer.c
@@ -66,6 +66,8 @@ static int flash_blink_wait_inc_max = DEFAULT_WAIT_INC_MAX;
 static int haptic_mode = 1; // 0 - always blink, 1 - only blink with haptic vibration notifications
 static int flash_only_face_down = 1;
 
+static bool in_call = false;
+
 static bool flash_start_queued = false;
 
 static int get_flash_ignore_vibration(void) {
@@ -427,7 +429,7 @@ void do_flash_blink(void) {
 	pr_info("%s ########################## flash_blink ############################# \n",__func__);
 	alarm_cancel(&flash_blink_unidle_smp_cpu_rtc); // stop pending alarm... no need to unidle cpu in that alarm...
 
-	if (currently_torch_mode || interrupt_retime) return;
+	if (currently_torch_mode || interrupt_retime || in_call) return;
 
 	dim = is_dim_blink_needed();
 	pr_info("%s dim %d\n",__func__,dim);
@@ -620,6 +622,7 @@ void flash_blink(bool haptic) {
 	// if not a haptic notificcation and haptic blink mode on, do not do blinking...
 	if (!haptic && uci_get_flash_haptic_mode()) return;
 	if (haptic && get_flash_ignore_vibration()) return;
+	if (in_call) return;
 
 	// if torch i on, don't blink
 	if (currently_torch_mode) return;
@@ -747,6 +750,12 @@ static void ntf_listener(char* event, int num_param, char* str_param) {
 			flash_blink(true);
 		} else {
 			flash_stop_blink();
+		}
+	}
+	if (!strcmp(event,NTF_EVENT_IN_CALL)) { // call started
+		in_call = !!num_param;
+		if (in_call) {
+			flash_stop_blink(); // stop flash/vib reminder
 		}
 	}
 
